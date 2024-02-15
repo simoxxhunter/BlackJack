7fa3fe5e25e30cbebd4700197e0702fff87fe38f
simoxxhunter <94250469+simoxxhunter@users.noreply.github.com>
2024-02-15 14:21:58 +0100

    Main Menu + classes

---
+++ Player.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.blackjackgame;
+
+
+public class Player {
+    public static String player_name;
+    public static Float player_balance;
+    public static Float player_hand;
+   
+}
